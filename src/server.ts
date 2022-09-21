68dd812340963ae585848214f816d475535e968d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-22 06:19:38 +0900

    convert sample.md

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import livereload from "livereload";
+import fs from "fs";
+import { convertFile } from "./convert";
 
-const public_dir = `${__dirname}/../public`;
+const publicDir = `${__dirname}/../public`;
+const sourceFile = `${__dirname}/../sample.md`;
+const destFile = `${publicDir}/index.html`;
+
+fs.watchFile(sourceFile, { persistent: true, interval: 10 }, () => {
+  convertFile(sourceFile, destFile);
+});
 
 const app = express();
-
-app.use("/", express.static(public_dir));
-
+app.use("/", express.static(publicDir));
 app.listen(3000);
 
 const server = livereload.createServer();
-server.watch(public_dir);
+server.watch(publicDir);
