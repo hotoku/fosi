843994c65c3c58a186c903633d7b03b001fe1db6
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-22 04:24:04 +0900

    wip: serves

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import livereload from "livereload";
+
+const public_dir = `${__dirname}/../public`;
 
 const app = express();
 
-app.use("/", express.static(`${__dirname}/../public`));
+app.use("/", express.static(public_dir));
 
 app.listen(3000);
+
+const server = livereload.createServer();
+server.watch(public_dir);
