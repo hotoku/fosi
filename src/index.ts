ca742d31fdb7ceffde5de1c87f2aa23f834422fd
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-24 14:06:23 +0900

    remove main

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
 #!/usr/bin/env node
 
-const main = require("./main");
+import yargs from "yargs";
+import path from "path";
+import { launchServers } from "./server";
+import { TargetFileExists } from "./exceptions";
+
+const parseArguments = () => {
+  const argv = yargs(process.argv.slice(2))
+    .options({
+      i: { type: "string", demandOption: true, describe: "input file" },
+      d: {
+        type: "string",
+        describe:
+          "output file. if not given, index.html in the same folder as input file.",
+      },
+      f: { type: "boolean", describe: "overwrite existing output" },
+    })
+    .parseSync();
+  return argv;
+};
+
+const main = () => {
+  const argv = parseArguments();
+  const sourceFile = path.resolve(argv.i);
+  try {
+    launchServers(sourceFile, { force: argv.f, output: argv.d });
+  } catch (e: any) {
+    if (e instanceof TargetFileExists) {
+      console.log(e.message);
+      console.log(
+        "If you want to over write the file, start me with -f option."
+      );
+    }
+  }
+};
 
 main();
